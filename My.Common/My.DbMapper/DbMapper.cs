c5ab4ddc3d6ca155cace03c201d9703bf36574c4
de_fault <alexizual1@gmail.com>
2020-10-21 15:14:24 +0500

    add dbmapper and up version project

---
+++ My.Common/My.DbMapper/DbMapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace My.DbMapper
+{
+    public class DbMapper
+    {
+        public T ExecuteNonQuery<T>(string query, object parameters)
+        {
+            return default(T);
+        }
+    }
+}
